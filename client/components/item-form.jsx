d9cde6663c8e7c082313a3d8f13504778b08ffc7
Tesia Tran <tesiatran88@gmail.com>
2019-09-09 20:36:08 -0700

    Added a render method that returns a form, input, and div with a button inside.

--- client/components/item-form.jsx
+++ client/components/item-form.jsx
@@ -8,6 +8,24 @@ class ItemForm extends React.Component {
       status: false
     };
   }
+
+  render() {
+    return (
+      <form className="input-group mb-4 shadow-sm" onSubmit={this.handleSubmit}>
+        <input
+          required
+          autoFocus
+          type="text"
+          item={this.state.item}
+          className="form-control"
+          placeholder="Add a new item."
+          onChange={this.handleChange}/>
+        <div className="input-group-append">
+          <button type="submit" className="btn btn-primary">Add</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default ItemForm;
