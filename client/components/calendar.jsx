9fb9c5f055df9a07497e5f76e62a16b6fc51681f
jasethomson <jasethomson31@gmail.com>
2019-09-11 08:50:02 -0700

    Added TableBody component to holder table body contents for Calendar.

--- client/components/calendar.jsx
+++ client/components/calendar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TableBody from "./calendar-table-body";
 
 class Calendar extends React.Component {
   constructor(props){
@@ -87,50 +88,7 @@ class Calendar extends React.Component {
                 <th scope="col">Dinner</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <th scope="row" className="2019-09-08" >Sun<br /> 08</th>
-                <td className="breakfast" onClick={this.handleClick}>{this.state.meal[0].label}</td>
-                <td className="lunch" onClick={this.handleClick}>{this.state.meal[1].label}</td>
-                <td className="dinner" onClick={this.handleClick}>{this.state.meal[2].label}</td>
-              </tr>
-              <tr>
-                <th scope="row" className="2019-09-09">Mon<br />09</th>
-                <td className="breakfast" onClick={this.handleClick}>{this.state.meal[3].label}</td>
-                <td className="lunch" onClick={this.handleClick}>{this.state.meal[4].label}</td>
-                <td className="dinner" onClick={this.handleClick}>{this.state.meal[5].label}</td>
-              </tr>
-              <tr>
-                <th scope="row" className="2019-09-10">Tues<br />10</th>
-                <td className="breakfast" onClick={this.handleClick}>{this.state.meal[6].label}</td>
-                <td className="lunch" onClick={this.handleClick}>{this.state.meal[7].label}</td>
-                <td className="dinner" onClick={this.handleClick}>{this.state.meal[8].label}</td>
-              </tr>
-              <tr>
-                <th scope="row" className="2019-09-11">Wed<br />11</th>
-                <td className="breakfast" onClick={this.handleClick}>{this.state.meal[9].label}</td>
-                <td className="lunch" onClick={this.handleClick}>{this.state.meal[10].label}</td>
-                <td className="dinner" onClick={this.handleClick}>{this.state.meal[11].label}</td>
-              </tr>
-              <tr>
-                <th scope="row" className="2019-09-12">Thurs<br />12</th>
-                <td className="breakfast" onClick={this.handleClick}>{this.state.meal[12].label}</td>
-                <td className="lunch" onClick={this.handleClick}>{this.state.meal[13].label}</td>
-                <td className="dinner" onClick={this.handleClick}>{this.state.meal[14].label}</td>
-              </tr>
-              <tr>
-                <th scope="row" className="2019-09-13">Fri<br />13</th>
-                <td className="breakfast" onClick={this.handleClick}>{this.state.meal[15].label}</td>
-                <td className="lunch" onClick={this.handleClick}>{this.state.meal[16].label}</td>
-                <td className="dinner" onClick={this.handleClick}>{this.state.meal[17].label}</td>
-              </tr>
-              <tr>
-                <th scope="row" className="2019-09-14">Sat<br />14</th>
-                <td className="breakfast" onClick={this.handleClick}>{this.state.meal[18].label}</td>
-                <td className="lunch" onClick={this.handleClick}>{this.state.meal[19].label}</td>
-                <td className="dinner" onClick={this.handleClick}>{this.state.meal[20].label}</td>
-              </tr>
-            </tbody>
+            <TableBody handleClick={this.handleClick} meal={this.state.meal}/>
           </table>
           <form className="form-inline text-align-center" onSubmit={this.handleSubmit}>
             <div className="form-group mx-sm-3 mb-2 mr-2 ml-5">
