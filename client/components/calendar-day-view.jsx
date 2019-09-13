ba5fd83b7841d9b59a66861ed577b0d31fef1570
jasethomson <jasethomson31@gmail.com>
2019-09-13 10:49:34 -0700

    Added Month/year to top, week view button to switch between week view and day view, and added breakfast, lunch and dinner to page with hard coded values and bootstrap classes to space them.

--- client/components/calendar-day-view.jsx
+++ client/components/calendar-day-view.jsx
@@ -8,7 +8,27 @@ class DayCalendar extends React.Component {
     }
   }
   render(){
-    return null;
+    return (
+      <div>
+        <h3 className="text-center">September, 2019</h3>
+        <button className="btn btn-link" onClick={() => { this.props.setView('calendar', {}) }}>Week View</button>
+        <div className="container d-flex">
+          <div className="row">
+            <div className="col-md-4 mb-3">Breakfast</div>
+            <div className="col-md-4 offset-md-2 mb-3">Lunch</div>
+            <div className="col-md-4 offset-md-2 mb-3">Dinner</div>
+          </div>
+          <div className="row">
+            <div className="col-md-3 offset-md-3 mb-3">Burrito</div>
+            <div className="col-md-3 offset-md-3 mb-3">Pizza</div>
+            <div className="col-md-3 offset-md-3 mb-3">Sushi</div>
+          </div>
+          {/* <div className="row">
+            <div className="col-md-6 offset-md-3">Dinner</div>
+          </div> */}
+        </div>
+      </div>
+    )
   }
 }
 
