0cdc785aad975088903c16e427f7be14b31613bd
jasethomson <jasethomson31@gmail.com>
2019-09-16 10:31:45 -0700

    Added stateful User-info component

---
+++ client/components/user-info.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class UserInfo extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: ""
+    }
+  }
+  render(){
+    return (
+      null
+    )
+  }
+}
+
+export default UserInfo;
