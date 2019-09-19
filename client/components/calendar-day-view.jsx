1b87c8b38a74bb51df5aa42e65f4ebdfbaa8d997
cresia <kimkim4@gmail.com>
2019-09-19 11:12:20 -0700

    calendar and modal

--- client/components/calendar-day-view.jsx
+++ client/components/calendar-day-view.jsx
@@ -15,16 +15,16 @@ function CalendarDayView(props){
             <div className="col-2 mealTitle">
               Breakfast
             </div>
-            <div className="col-6">
-              <div className=" card border-dark col mealRecipe" >{props.meal[props.mealObj.breakfast].recipe_label}</div>
+            <div className="col-6 mealRecipe ">
+              <div>{props.meal[props.mealObj.breakfast].recipe_label}</div>
             </div>
           </div>
            <div className="row justify-content-center">
             <div className="col-2 mealTitle">
               Lunch
             </div>
-            <div className="col-6">
-               <div className="card border-dark col mealRecipe">{props.meal[props.mealObj.lunch].recipe_label}</div>
+            <div className="col-6 mealRecipe">
+               <div>{props.meal[props.mealObj.lunch].recipe_label}</div>
             </div>
           </div>
 
@@ -32,27 +32,13 @@ function CalendarDayView(props){
             <div className="col-2 mealTitle">
               Dinner
             </div>
-            <div className="col-6">
-              <div className="card border-dark col mealRecipe">{props.meal[props.mealObj.dinner].recipe_label}</div>
+            <div className="col-6 mealRecipe">
+              <div>{props.meal[props.mealObj.dinner].recipe_label}</div>
             </div>
       
           </div>
 
-          
-{/*                 
-        <div className="container d-flex">
-          <div className="row">
-            <div className="col-md-4 mb-3">Breakfast</div>
-            <div className="col-md-4 offset-md-2 mb-3">Lunch</div>
-            <div className="col-md-4 offset-md-2 mb-3">Dinner</div>
-          </div>
-          <div className="row">
-            <div className="col-md-3 offset-md-3 mb-3">{props.meal[props.mealObj.breakfast].recipe_label}</div>
-            <div className="col-md-3 offset-md-3 mb-3">{props.meal[props.mealObj.lunch].recipe_label}</div>
-            <div className="col-md-3 offset-md-3 mb-3">{props.meal[props.mealObj.dinner].recipe_label}</div>
-          </div>
-        </div>
- */}
+        
 
 
       </div>
